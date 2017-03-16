9d7efc9e726cce3445db6b4416888e6cb7537874
Kirill <kirill2001.91@mail.ru>
2017-03-16 22:18:29 +0300

    Создал проект

---
+++ Snowboard/Snowboard.Domain/Entities/Snowboard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snowboard.Domain.Entities
+{
+    public class Snowboard
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Cost { get; set; }
+        public byte[] Picture { get; set; }
+        public int Number { get; set; }
+    }
+    //https://metanit.com/sharp/helpdeskmvc/2.1.php
+}
---
+++ Snowboard/Snowboard.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Snowboard.WebUI.MvcApplication" Language="C#" %>
