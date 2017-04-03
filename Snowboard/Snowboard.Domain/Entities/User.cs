aec991a8513a0fe84a2d8779836e616364dfdb46
Kirill <kirill2001.91@mail.ru>
2017-04-03 23:32:58 +0300

    home

---
+++ Snowboard/Snowboard.Domain/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snowboard.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        [Required]
+        [Display(Name = "Имя")]
+        [MaxLength(50, ErrorMessage = "Максимальная длинна имени 50 символов")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Телефон")]
+        public int Phone { get; set; }
+    }
+}
